aeab816030199e8bd3325bc2b52451a561121613
tugonbob <joshuakgao@gmail.com>
2022-08-27 23:56:54 -0500

    Home page completed

--- src/components/header/logo.tsx
+++ src/components/header/logo.tsx
@@ -4,7 +4,7 @@ import watermelon from "../../assets/images/watermelon.png";
 export default function Logo() {
   return (
     <div className="row">
-      <img className="logo" src={watermelon} alt="Logo" />
+      <img className="logo" src={watermelon} />
       <h4 className="tertiary" style={{ marginLeft: 10 }}>
         Gao
       </h4>
