c6cb0d429bbdd6624a3ace400fb6c93ea4cd0ecf
tugonbob <joshuakgao@gmail.com>
2022-08-30 22:52:13 -0500

    Added some placeholder pages

---
+++ src/screens/BusinessPage/BusinessPage.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export function BusinessPage() {
+  return <div>BusinessPage</div>;
+}
---
+++ src/screens/SoftwarePage/SoftwarePage.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export function SoftwarePage() {
+  return <div>SoftwarePage</div>;
+}
