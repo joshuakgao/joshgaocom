68b3c12c7c6ae36bf5ab7cf0464e25b8f0b965fb
tugonbob <joshuakgao@gmail.com>
2022-09-09 17:23:16 -0500

    Created chatbotBob title card

---
+++ src/components/projectComponents/software/chatbotBob/chatbotBobCard.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { TitleCard } from "../../titleCard";
+import chatbot_background from "../../../../assets/projects/software/chatbotBob/chatbot_title_card.png";
+
+export function ChatbotBobCard({
+  toFullscreen = false,
+}: {
+  toFullscreen?: boolean;
+}) {
+  return (
+    <TitleCard
+      backgroundImage={chatbot_background}
+      textLocation={["bottom", "left"]}
+      to="/software/chatbotBob"
+      toFullscreen={toFullscreen}
+    >
+      <h1>Chatbot Bob</h1>
+    </TitleCard>
+  );
+}
--- src/pages/SoftwarePage/softwarePage.tsx
+++ src/pages/SoftwarePage/softwarePage.tsx
@@ -1,15 +1,11 @@
 import React from "react";
 import { ScrollDiv } from "../../components/commonComponents";
+import { ChatbotBobCard } from "../../components/projectComponents/software";
 
 export function SoftwarePage() {
   return (
     <ScrollDiv>
-      <iframe
-        allow="microphone;"
-        width="350"
-        height="430"
-        src="https://console.dialogflow.com/api-client/demo/embedded/b17d18f9-3b6a-48fc-8ea7-956717271319"
-      />
+      <ChatbotBobCard />
     </ScrollDiv>
   );
 }
