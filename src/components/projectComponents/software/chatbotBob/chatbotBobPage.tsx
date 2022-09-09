48c591bd3abfed5e91499e522126a05be42f6b34
tugonbob <joshuakgao@gmail.com>
2022-09-09 17:33:17 -0500

    Added Bob webdemo to chatbotBobPage

--- src/components/projectComponents/software/chatbotBob/chatbotBobPage.tsx
+++ src/components/projectComponents/software/chatbotBob/chatbotBobPage.tsx
@@ -1,11 +1,20 @@
 import React from "react";
-import { ScrollDiv } from "../../../commonComponents";
+import { MainContentDiv, ScrollDiv } from "../../../commonComponents";
 import { ChatbotBobCard } from "./chatbotBobCard";
 
 export function ChatbotBobPage() {
   return (
     <ScrollDiv>
       <ChatbotBobCard toFullscreen />
+      <MainContentDiv>
+        <h2>Talk to Bob</h2>
+        <iframe
+          allow="microphone;"
+          width="350"
+          height="430"
+          src="https://console.dialogflow.com/api-client/demo/embedded/b17d18f9-3b6a-48fc-8ea7-956717271319"
+        />
+      </MainContentDiv>
     </ScrollDiv>
   );
 }
