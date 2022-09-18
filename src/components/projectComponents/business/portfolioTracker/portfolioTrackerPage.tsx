a2fea9ffc8f56b5b24927731f184afc95193e486
tugonbob <joshuakgao@gmail.com>
2022-09-17 23:39:33 -0500

    Fixed Google Sheets embed formatting

--- src/components/projectComponents/business/portfolioTracker/portfolioTrackerPage.tsx
+++ src/components/projectComponents/business/portfolioTracker/portfolioTrackerPage.tsx
@@ -7,8 +7,30 @@ export function PortfolioTrackerPage() {
     <ScrollDiv>
       <PortfolioTrackerCard toFullscreen />
       <MainContentDiv>
-        <h1>hi</h1>
+        <h2>Introduction</h2>
+        <iframe
+          style={styles.iframe}
+          src="https://docs.google.com/spreadsheets/d/e/2PACX-1vQ_r0kZBuN8E-M-JmVtZQNNa-2UZmzNq0lOSG0YF4ic1o4EiHgGbApFApUizRJc6mPTVEaHFZt34dbx/pubhtml?widget=true&amp;autosize=true&amp;headers=false&amp;chrome=false"
+        />
+        <h2 style={{ marginTop: "75vh" }}>Tabs Explanations</h2>
       </MainContentDiv>
     </ScrollDiv>
   );
 }
+
+type StyleSheet = {
+  [key: string]: React.CSSProperties;
+};
+
+const styles: StyleSheet = {
+  iframe: {
+    position: "absolute",
+    left: "2vw",
+    height: "65vh",
+    width: "96vw",
+    maxWidth: 1615,
+    maxHeight: 675,
+    border: "1px solid lightgrey",
+    borderRadius: 10,
+  },
+};
