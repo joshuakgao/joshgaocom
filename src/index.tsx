2ddc2d1643943a56a4a88b1b8beab163b59fe414
tugonbob <joshuakgao@gmail.com>
2022-09-18 11:14:34 -0500

    Added scroll to top on navigation transition component

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { HashRouter } from "react-router-dom";
+import { ScrollToTop } from "./components/customComponents/scrollToTop/scrollToTop";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
@@ -9,6 +10,7 @@ const root = ReactDOM.createRoot(
 root.render(
   <React.StrictMode>
     <HashRouter>
+      <ScrollToTop />
       <App />
     </HashRouter>
   </React.StrictMode>
