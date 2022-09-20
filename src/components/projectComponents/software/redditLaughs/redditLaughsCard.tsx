6e996236cc64ef0bbdef0692830b54d520a5cea4
tugonbob <joshuakgao@gmail.com>
2022-09-20 18:46:53 -0500

    Created RedditLaughs card art

--- src/components/projectComponents/software/redditLaughs/redditLaughsCard.tsx
+++ src/components/projectComponents/software/redditLaughs/redditLaughsCard.tsx
@@ -1,17 +1,19 @@
 import { TitleCard } from "../../titleCard";
-import redditYoutube from "../../../../assets/projects/software/redditLaughs/redditYoutubeLogo.png";
+import redditYoutube from "../../../../assets/projects/software/redditLaughs/redditToYoutube.png";
 
 export function RedditLaughsCard({ toFullscreen }: { toFullscreen?: boolean }) {
   return (
     <TitleCard
       backgroundImage={redditYoutube}
-      backgroundSize="20vh"
-      backgroundColor="#e9e9e9"
+      backgroundSize="cover"
+      backgroundColor="#1e1e1e"
       toFullscreen={toFullscreen}
       to="/software/redditLaughs"
     >
-      <h1>Reddit Laughs</h1>
-      <h2>Automating a Youtube channel with Reddit content</h2>
+      <h1 className="primary">Reddit Laughs</h1>
+      <h2 className="primary">
+        Automating a Youtube channel with Reddit content
+      </h2>
     </TitleCard>
   );
 }
