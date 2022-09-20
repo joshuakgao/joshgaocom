9ecbc51a70292cb8faace5b02f43a800de31a1db
tugonbob <joshuakgao@gmail.com>
2022-09-20 18:10:51 -0500

    Completed redditLaughs content

--- src/components/projectComponents/software/redditLaughs/redditLaughsPage.tsx
+++ src/components/projectComponents/software/redditLaughs/redditLaughsPage.tsx
@@ -3,6 +3,7 @@ import {
   ContentHeader,
   MainContentDiv,
   ScrollDiv,
+  YoutubeEmbed,
 } from "../../../commonComponents";
 import { RedditLaughsCard } from "./redditLaughsCard";
 
@@ -19,15 +20,15 @@ export function RedditLaughsPage() {
         <h2>Introduction</h2>
         <p>
           With this project, I wanted to answer an essential question: can I be
-          a huge youtuber without any of the work? Can I get millions of views
-          and subscribers through automating the youtuber processes with some
+          a huge Youtuber without any of the work? Can I get millions of views
+          and subscribers through automating the Youtuber processes with some
           carefully created cleverness? Youtubers today are at the whims of the
           Youtube algorithm, which pressures creators to be constantly pumping
-          out entertaing videos, driving many Youtubers into creative burnout.
+          out entertaining videos, driving many Youtubers into creative burnout.
           I'm going to be exploring the idea of curating entertaining content on
           the internet and using a consistent video format to provide to
-          youtube. To do this, I scraped Reddit meme content and some scripted
-          video editing to create effortless meme video compliations.
+          Youtube. To do this, I scraped Reddit meme content and some scripted
+          video editing to create effortless meme video compilations.
         </p>
         <h2>Reddit Content Scraping</h2>
         <p>
@@ -35,37 +36,62 @@ export function RedditLaughsPage() {
           satisfy the Youtube algorithm overlords, as well as funny or
           interesting short form videos. Unfortunately, still image memes
           wouldn't be ideal here since they would require some text to speech
-          and maybe even hiding parts of the meme as to not ruin the punchline.
-          All items that would require manual editing. And who wants actually to
-          do work?
+          and maybe even hiding parts of the meme so as to not ruin the
+          punchline. All items that would require manual editing. And who
+          actually wants to do work?
         </p>
         <p>
-          To achieve this, I used the Praw {"("}Python Reddit API Wrapper{")"}{" "}
+          To achieve this, I used the Praw {"("}Python Reddit API Wrapper{") "}
           python package, and targeted the top posts of the week in the
           r/perfectlycutscreams subreddit. This content was perfect since all
           posts would be videos and all would "hopefully" generate some sort of
-          laugh or unexepectedness.
+          laugh or unexpectedness.
         </p>
         <h2>Python Video Editing</h2>
         <p>
-          After I gathered my content, I need to put them together to create the
-          full meme compliation video. There is this handy dandy python package
-          called Moviepy. Perfect for automating video editing. Using the
-          package, I simply made a bunch of video appends and slapped a "like
-          and subscribe" animation to the beginning of the video. And just like
-          that, a freshly minted Youtube video was born and ready to be
-          uploaded.
+          After I have gathered my content, I need to put them together to
+          create the full meme compilation video. There is this handy dandy
+          python package called MoviePy. Perfect for automating video editing.
+          Using the package, I simply made a bunch of video appends and slapped
+          a "like and subscribe" animation to the beginning of the video. And
+          just like that, a freshly minted Youtube video was born and ready to
+          be uploaded.
         </p>
         <h2>Other Thoughts</h2>
         <p>
           I could have taken the automation process to its fullest extent by
-          automatically running the script on every Monday and post the video
+          automatically running the script on every Monday and posting the video
           through the Youtube API. However, I decided against this because it
           would give me time to review the video for any nsfw content that the
           depths of Reddit can manufacture.
         </p>
-        <h2>Demo</h2>
+        <h2>Demonstration</h2>
+        <p>
+          Here, I show the process of creating a 60 second "perfectly cut
+          screams" video compilation. However, you can define your required
+          video length to be as long or short as you want, as well as which
+          subreddits to scrape from. This is all defined in the
+          "SelectedSubreddits.csv" file.
+        </p>
+        <YoutubeEmbed embedId="suo1BnijcpE" />
         <h2>Results</h2>
+        <p>
+          Even though this idea shows a lot of promise, it still requires a lot
+          more sophistication in its automation or simply more manual effort
+          from a human. There were issues like inconsistent video quality,
+          boring video clips, and even nsfw submissions as mentioned before.
+          Both of the latter items would require its own challenging machine
+          learning algorithms to classify all the videos accurately and
+          automatically. However, I am sure that I am not the first to think of
+          this idea, and Youtube channels that are heavily automated in a
+          similar fashion definitely exist, but also have a huge advantage in
+          producing consistent and entertaining content for the Youtube
+          audience.
+        </p>
+        <p>
+          Also, I figured out that borrowed content like this can't be monetized
+          on Youtube anyways. Dang...
+        </p>
       </MainContentDiv>
     </ScrollDiv>
   );
