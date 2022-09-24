ee61c29ddea63593eea15e53faa44ae753dff7ea
Joshua Gao <joshuagao@Joshuas-MacBook-Pro.local>
2022-09-23 21:02:22 -0500

    Started on zillow deal finder content

--- src/components/projectComponents/software/zillowDealFinder/zillowDealFinderPage.tsx
+++ src/components/projectComponents/software/zillowDealFinder/zillowDealFinderPage.tsx
@@ -1,5 +1,9 @@
 import React from "react";
-import { MainContentDiv, ScrollDiv } from "../../../commonComponents";
+import {
+  ContentHeader,
+  MainContentDiv,
+  ScrollDiv,
+} from "../../../commonComponents";
 import { ZillowDealFinderCard } from "./zillowDealFinderCard";
 
 export function ZillowDealFinderPage() {
@@ -7,8 +11,44 @@ export function ZillowDealFinderPage() {
     <ScrollDiv>
       <ZillowDealFinderCard toFullscreen />
       <MainContentDiv>
-        <h1>Introduction</h1>
+        <ContentHeader
+          date="20/6/2022"
+          skills={["python", "googleDrive"]}
+          projectComponents={"Zillow Data Scraping - Proxies - Google Sheets"}
+          sources={{
+            github: "https://github.com/tugonbob/ZillowListingsScraper.git",
+            googleDrive:
+              "https://docs.google.com/spreadsheets/d/1qD2yjneLiJczgDRSSvbo4YF5NHOBaaOxutOvw86UOtg/edit?usp=sharing",
+          }}
+        />
+        <h2>Summary</h2>
+        <h2>Introduction</h2>
+        <iframe
+          style={styles.iframe}
+          src="https://docs.google.com/spreadsheets/d/e/2PACX-1vRV3VF8HIPGQal3m6bUSKfPrIRf1bDqNiCrDDPJ2g50TkPMmXnPbYV-k1v19l-yHt6os7fTJ8kcHSzZ/pubhtml?widget=true&amp;headers=false"
+        />
+        <h2>Zillow Data Scraping</h2>
+        <h2>Taking on Bot Protection with Proxies</h2>
+        <h2>Analayzing Data</h2>
+        <h2>Conclusion</h2>
       </MainContentDiv>
     </ScrollDiv>
   );
 }
+
+type StyleSheet = {
+  [key: string]: React.CSSProperties;
+};
+
+const styles: StyleSheet = {
+  iframe: {
+    position: "absolute",
+    left: "2vw",
+    height: "65vh",
+    width: "96vw",
+    maxWidth: 1615,
+    maxHeight: 675,
+    border: "1px solid lightgrey",
+    borderRadius: 10,
+  },
+};
