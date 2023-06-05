78ad9cc5f6e43f35f10747b467045f878372f572
tugonbob <joshuakgao@gmail.com>
2023-06-05 18:42:42 -0500

    Added animation to homepage

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,20 +1,23 @@
 import React from "react";
 import me from "../assets/images/me.jpg";
 import { ScrollDiv } from "../components/commonComponents";
+import "../assets/styles.css";
 
 export function HomePage() {
   return (
     <ScrollDiv style={styles.container}>
       <div style={styles.textContainer}>
-        <h1>Hey, I'm Joshua Gao</h1>
-        <h4 style={styles.subtitle} className="fade-up">
+        <h1 style={{ fontSize: "calc(16px + 2vw)" }} className="fade-in-0">
+          Hey, I'm Joshua Gao
+        </h1>
+        <h4 style={styles.subtitle} className="fade-in-3">
           I am a Machine Learning Engineer based in Texas. I love analyzing and
           visualizing large amounts of data and providing unique and helpful
           insights. Some of my other interests include playing volleyball and
           writing music.
         </h4>
       </div>
-      <img style={styles.img} src={me} />
+      <img style={styles.img} src={me} className="fade-in-4" />
     </ScrollDiv>
   );
 }
@@ -33,7 +36,7 @@ const styles: StyleSheet = {
     minHeight: "98.5vh",
   },
   textContainer: {
-    maxWidth: 500,
+    maxWidth: 422,
     marginTop: 50,
   },
   title: {
@@ -42,13 +45,14 @@ const styles: StyleSheet = {
   subtitle: {
     marginTop: 8,
     textAlign: "justify",
+    fontSize: "calc(16px + 0.5vw)",
   },
   img: {
     borderRadius: "var(--borderRadius)",
     marginTop: 32,
     marginBottom: 32,
     width: "100%",
-    maxWidth: 450,
+    maxWidth: 422,
     minWidth: 200,
     height: undefined,
   },
