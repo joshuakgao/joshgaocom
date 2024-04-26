3c3e13fcceae10c86383cd08d881e228dc9cc496
Joshua Gao <joshuakgao@gmail.com>
2024-04-26 22:43:43 +0000

    trying ssl

--- server/api.py
+++ server/api.py
@@ -63,4 +63,5 @@ def validate_api_key():
     return True, None
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", debug=True, port='8080')
+    app.run(host="0.0.0.0", ssl_context=('cert.pem', 'key.pem'))
+    # app.run(host="0.0.0.0", debug=True, port='8080')
