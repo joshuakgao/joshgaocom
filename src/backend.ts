678bc1de6836415b83a202a66b65ff2c19ae8a24
tugonbob <57576522+tugonbob@users.noreply.github.com>
2024-04-27 17:30:51 -0500

    Change backend url

--- src/backend.ts
+++ src/backend.ts
@@ -1,4 +1,4 @@
 export const backend = {
-  // url: "https://tugonbob.pythonanywhere.com",
-  url: "http://127.0.0.1:5000",
+  url: "https://tugonbob.pythonanywhere.com",
+  // url: "http://127.0.0.1:5000",
 };
