a6bdd9f4b32b495bd784661ba785398b37538122
tugonbob <57576522+tugonbob@users.noreply.github.com>
2024-07-05 17:29:00 -0500

    Import error

--- server/aiml/gpt_nano/inference.py
+++ server/aiml/gpt_nano/inference.py
@@ -1,12 +1,21 @@
 import torch
 from torch.nn import functional as F
-from .model import GPT, GPTConfig
+from .model import GPT
 import tiktoken
 from transformers import GPT2LMHeadModel
 import os
+from dataclasses import dataclass
 
 # Global Variables
 DEVICE = 'cpu'
+
+@dataclass
+class GPTConfig:
+    block_size: int = 1024 # max sequence length
+    vocab_size: int = 50257 # number of tokens: 50,000 BPE merges + 256 bytes tokens + 1 <|endoftext|> token
+    n_layer: int = 12 # number of layers
+    n_head: int = 12 # number of heads
+    n_embd: int = 768 # embedding dimension
 
 
 def load_gpt_nano_model(model_path):
--- server/aiml/gpt_nano/model.py
+++ server/aiml/gpt_nano/model.py
@@ -1,4 +1,5 @@
 import inspect
+from dataclasses import dataclass
 import torch
 import torch.nn as nn
 from torch.nn import functional as F
@@ -67,27 +68,13 @@ class Block(nn.Module):
         return x
 
 
-class GPTConfig():
-  """
-  Configuration for GPT model.
-  """
-
-  def __init__(self, block_size=1024, vocab_size=50257, n_layer=12, n_head=12, n_embd=768):
-    """
-    Initializes the GPTConfig with the given parameters.
-
-    Args:
-      block_size: Maximum sequence length.
-      vocab_size: Size of the vocabulary.
-      n_layer: Number of layers in the model.
-      n_head: Number of heads in the attention mechanism.
-      n_embd: Embedding dimension.
-    """
-    self.block_size = block_size
-    self.vocab_size = vocab_size
-    self.n_layer = n_layer
-    self.n_head = n_head
-    self.n_embd = n_embd
+@dataclass
+class GPTConfig:
+    block_size: int = 1024 # max sequence length
+    vocab_size: int = 50257 # number of tokens: 50,000 BPE merges + 256 bytes tokens + 1 <|endoftext|> token
+    n_layer: int = 12 # number of layers
+    n_head: int = 12 # number of heads
+    n_embd: int = 768 # embedding dimension
 
 
 class GPT(nn.Module):
