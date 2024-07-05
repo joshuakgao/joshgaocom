74b80e824220499e5068dd4db87dd8068edc0569
tugonbob <57576522+tugonbob@users.noreply.github.com>
2024-07-05 00:37:06 -0500

    Use relative imports

--- server/aiml/gpt_nano/inference.py
+++ server/aiml/gpt_nano/inference.py
@@ -1,6 +1,6 @@
 import torch
 from torch.nn import functional as F
-from model import GPT, GPTConfig
+from .model import GPT, GPTConfig
 import tiktoken
 from transformers import GPT2LMHeadModel
 import os
