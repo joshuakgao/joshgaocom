7f520529ece3dff9ae5fab570127f19673c7db13
tugonbob <57576522+tugonbob@users.noreply.github.com>
2024-07-05 18:38:19 -0500

    Giving up on Civilfinetune model

--- server/aiml/gpt_nano/inference.py
+++ server/aiml/gpt_nano/inference.py
@@ -1,6 +1,6 @@
 import torch
 from torch.nn import functional as F
-import os
+import os, sys
 from aiml.gpt_nano.model import GPTConfig, GPT
 import tiktoken
 from transformers import GPT2LMHeadModel
@@ -62,21 +62,21 @@ def query_model(model, query, max_length=100, num_return_sequences = 1):
 def compare_gpt_models(query, max_length=100):
     base_dir = os.path.dirname(__file__)
     gpt_nano_path = os.path.join(base_dir, 'models', 'model_19072.pt')
-    civil_finetune_path = os.path.join(base_dir, 'models', 'civil_finetune_v3_model_00200.pt')
+    # civil_finetune_path = os.path.join(base_dir, 'models', 'civil_finetune_v3_model_00200.pt')
 
     gpt_nano_model = load_gpt_nano_model(gpt_nano_path)
     gpt_2_model = load_gpt_2_model()
-    civil_finetune_model = load_gpt_nano_model(civil_finetune_path)
+    # civil_finetune_model = load_gpt_nano_model(civil_finetune_path)
 
     query = query.strip()
     gpt_nano_response = query_model(gpt_nano_model, query, max_length=max_length)
     gpt_2_response = query_model(gpt_2_model, query, max_length=max_length)
-    civil_finetune_response = query_model(civil_finetune_model, query, max_length=max_length)
+    # civil_finetune_response = query_model(civil_finetune_model, query, max_length=max_length)
 
     return {
         "gpt_nano": gpt_nano_response,
         "gpt_2": gpt_2_response,
-        "civil_finetune": civil_finetune_response
+        # "civil_finetune": civil_finetune_response
     }
 
 
