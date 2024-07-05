a1f1d077d425893e958380c5f2735fd0722c60d5
tugonbob <57576522+tugonbob@users.noreply.github.com>
2024-07-05 17:24:50 -0500

    Removed dataclass decorator

--- server/aiml/gpt_nano/model.py
+++ server/aiml/gpt_nano/model.py
@@ -1,5 +1,4 @@
 import inspect
-from dataclasses import dataclass
 import torch
 import torch.nn as nn
 from torch.nn import functional as F
@@ -68,13 +67,27 @@ class Block(nn.Module):
         return x
 
 
-@dataclass
 class GPTConfig:
-    block_size: int = 1024 # max sequence length
-    vocab_size: int = 50257 # number of tokens: 50,000 BPE merges + 256 bytes tokens + 1 <|endoftext|> token
-    n_layer: int = 12 # number of layers
-    n_head: int = 12 # number of heads
-    n_embd: int = 768 # embedding dimension
+  """
+  Configuration for GPT model.
+  """
+
+  def __init__(self, block_size=1024, vocab_size=50257, n_layer=12, n_head=12, n_embd=768):
+    """
+    Initializes the GPTConfig with the given parameters.
+
+    Args:
+      block_size: Maximum sequence length.
+      vocab_size: Size of the vocabulary.
+      n_layer: Number of layers in the model.
+      n_head: Number of heads in the attention mechanism.
+      n_embd: Embedding dimension.
+    """
+    self.block_size = block_size
+    self.vocab_size = vocab_size
+    self.n_layer = n_layer
+    self.n_head = n_head
+    self.n_embd = n_embd
 
 
 class GPT(nn.Module):
