b7515d3c36d8b90ba85f3fd54c103bfc4b6213cb
tugonbob <joshuakgao@gmail.com>
2024-07-14 16:34:52 -0500

    Don't display response after end of text token

--- server/aiml/gpt_nano/inference.py
+++ server/aiml/gpt_nano/inference.py
@@ -12,36 +12,46 @@ import time
 DEVICE = detect_device()
 
 
+def remove_after_string(text, target_string):
+    # Find the position of the target string
+    position = text.find(target_string)
+    if position != -1:
+        # Return the text up to (but not including) the target string
+        return text[:position]
+    # If the target string is not found, return the original text
+    return text
+
+
 def load_gpt_nano_model(model_path):
     model = GPT(GPTConfig(vocab_size=50304))
-    model.load_state_dict(torch.load(model_path, map_location=DEVICE)['model'])
+    model.load_state_dict(torch.load(model_path, map_location=DEVICE)["model"])
     model.eval()
     model.to(DEVICE)
     return model
 
 
 def load_gpt_2_model():
-    model = GPT2LMHeadModel.from_pretrained("gpt2") # 124M
+    model = GPT2LMHeadModel.from_pretrained("gpt2")  # 124M
     model.eval()
     model.to(DEVICE)
     return model
 
 
-def query_model(model, query, max_tokens=100, num_return_sequences = 1):
+def query_model(model, query, max_tokens=100, num_return_sequences=1):
     # prep model input
-    enc = tiktoken.get_encoding('gpt2')
+    enc = tiktoken.get_encoding("gpt2")
     tokens = enc.encode(query)
-    tokens = torch.tensor(tokens, dtype=torch.long) # (8,)
-    tokens = tokens.unsqueeze(0).repeat(num_return_sequences, 1) # (5, 8)
+    tokens = torch.tensor(tokens, dtype=torch.long)  # (8,)
+    tokens = tokens.unsqueeze(0).repeat(num_return_sequences, 1)  # (5, 8)
     x = tokens.to(DEVICE)
 
     # generate!
-    while x.size(1) < max_tokens: # max_tokens=30
+    while x.size(1) < max_tokens:  # max_tokens=30
         # forward the model to get the logits
         with torch.no_grad():
-            logits = model(x)[0] # (B, T, vocab_size)
+            logits = model(x)[0]  # (B, T, vocab_size)
             # take the logits at the last position
-            logits = logits[:, -1, :] # (B, vocab_size)
+            logits = logits[:, -1, :]  # (B, vocab_size)
             # get the probabilities
             probs = F.softmax(logits, dim=-1)
             # do top-k sampling of 50 (huggingface pipeline default)
@@ -49,9 +59,9 @@ def query_model(model, query, max_tokens=100, num_return_sequences = 1):
             topk_probs, topk_indices = torch.topk(probs, 50, dim=-1)
             # select a token from the top-k probabilities
             # note: multinomial does not demand the input to sum to 1
-            ix = torch.multinomial(topk_probs, 1) # (B, 1)
+            ix = torch.multinomial(topk_probs, 1)  # (B, 1)
             # gather the corresponding indices
-            xcol = torch.gather(topk_indices, -1, ix) # (B, 1)
+            xcol = torch.gather(topk_indices, -1, ix)  # (B, 1)
             # append to the sequence
             x = torch.cat((x, xcol), dim=1)
 
@@ -63,7 +73,7 @@ def query_model(model, query, max_tokens=100, num_return_sequences = 1):
 
 def compare_gpt_models(query, max_tokens=100):
     base_dir = os.path.dirname(__file__)
-    gpt_nano_path = os.path.join(base_dir, 'models', 'model_19072.pt')
+    gpt_nano_path = os.path.join(base_dir, "models", "model_19072.pt")
     # civil_finetune_path = os.path.join(base_dir, 'models', 'civil_finetune_v3_model_00200.pt')
 
     gpt_nano_model = load_gpt_nano_model(gpt_nano_path)
@@ -74,6 +84,11 @@ def compare_gpt_models(query, max_tokens=100):
     gpt_nano_response = query_model(gpt_nano_model, query, max_tokens=max_tokens)
     gpt_2_response = query_model(gpt_2_model, query, max_tokens=max_tokens)
     # civil_finetune_response = query_model(civil_finetune_model, query, max_tokens=max_tokens)
+
+    # remove string after <|endoftext|> token
+    eot_token = "<|endoftext|>"
+    gpt_nano_response = remove_after_string(gpt_nano_response, eot_token)
+    # civil_finetune_response = remove_after_string(gpt_nano_response, eot_token)
 
     return {
         "gpt_nano": gpt_nano_response,
@@ -87,4 +102,4 @@ if __name__ == "__main__":
     response = compare_gpt_models("Once upon a time", 50)
     end_time = time.time()
     print(response)
-    print(end_time-start_time)
+    print(end_time - start_time)
