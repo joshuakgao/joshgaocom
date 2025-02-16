2ef8415d6a09ecfc0bed477e4e87fa412c45e39f
tugonbob <57576522+tugonbob@users.noreply.github.com>
2025-02-16 14:08:37 -0600

    added roboto

--- components/ui/typography.tsx
+++ components/ui/typography.tsx
@@ -20,78 +20,78 @@ const createComponent = <T extends HTMLElement>(
 
 export const H1 = createComponent<HTMLHeadingElement>(
   "h1",
-  "scroll-m-20 text-2xl font-bold tracking-tight lg:text-2xl",
+  "scroll-m-20 text-2xl font-bold tracking-tight lg:text-2xl font-roboto",
   "H1"
 );
 
 export const H2 = createComponent<HTMLHeadingElement>(
   "h2",
-  "scroll-m-20 py-2 text-lg font-medium tracking-tight first:mt-0",
+  "scroll-m-20 py-2 text-lg font-medium tracking-tight first:mt-0 font-roboto",
   "H2"
 );
 
 export const H3 = createComponent<HTMLHeadingElement>(
   "h3",
-  "scroll-m-20 text-lg font-bold tracking-tight",
+  "scroll-m-20 text-lg font-bold tracking-tight font-roboto",
   "H3"
 );
 
 export const H4 = createComponent<HTMLHeadingElement>(
   "h4",
-  "scroll-m-20 text-md font-medium tracking-tight",
+  "scroll-m-20 text-md font-medium tracking-tight font-roboto",
   "H4"
 );
 
 export const Lead = createComponent<HTMLParagraphElement>(
   "p",
-  "text-lg text-gray-600",
+  "text-lg text-gray-600 font-roboto",
   "Lead"
 );
 
 export const P = createComponent<HTMLParagraphElement>(
   "p",
-  "leading-relaxed mt-6",
+  "leading-relaxed mt-6 font-roboto",
   "P"
 );
 
 export const Large = createComponent<HTMLDivElement>(
   "div",
-  "text-lg font-medium",
+  "text-lg font-medium font-roboto",
   "Large"
 );
 
 export const Small = createComponent<HTMLParagraphElement>(
   "p",
-  "text-sm font-normal leading-tight",
+  "text-sm font-normal leading-tight font-roboto",
   "Small"
 );
 
 export const Muted = createComponent<HTMLSpanElement>(
   "span",
-  "text-sm text-gray-500",
+  "text-sm text-gray-500 font-roboto",
   "Muted"
 );
 
 export const InlineCode = createComponent<HTMLSpanElement>(
   "code",
-  "relative rounded bg-gray-100 px-1 py-0.5 font-mono text-sm font-medium",
+  "relative rounded bg-gray-100 px-1 py-0.5 font-mono text-sm font-medium font-roboto",
   "InlineCode"
 );
 
 export const MultilineCode = createComponent<HTMLPreElement>(
   "pre",
-  "relative rounded bg-gray-100 p-4 font-mono text-sm font-medium overflow-x-auto",
+  "relative rounded bg-gray-100 p-4 font-mono text-sm font-medium overflow-x-auto font-roboto",
   "MultilineCode"
 );
 
 export const List = createComponent<HTMLUListElement>(
   "ul",
-  "my-4 ml-4 list-disc space-y-2",
+  "my-4 ml-4 list-disc space-y-2 font-roboto",
   "List"
 );
 
 export const Quote = createComponent<HTMLQuoteElement>(
   "blockquote",
-  "mt-4 border-l-4 pl-4 italic text-gray-600",
+  "mt-4 border-l-4 pl-4 italic text-gray-600 font-roboto",
   "Quote"
 );
