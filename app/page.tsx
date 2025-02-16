259163a076a53f5943122c7e28aa95370aaf284e
tugonbob <57576522+tugonbob@users.noreply.github.com>
2025-02-16 12:20:11 -0600

    Added placeholder career links

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,13 @@
 "use client";
 
-import me from "@/assets/images/me.jpg";
 import {
   Col,
   H1,
   H2,
-  H4,
-  Row,
-  Spacer,
-  ScrollDiv,
-  ScreenSizeIndicator,
   H3,
+  ScreenSizeIndicator,
+  ScrollDiv,
 } from "@/components/ui";
-import Image from "next/image";
 import ProjectItem from "@/components/ui/projectItem";
 import { useMediaQuery } from "react-responsive";
 
@@ -24,14 +19,34 @@ export default function Home() {
       <ScreenSizeIndicator />
       {isMobile ? (
         <>
-          <div className="pt-[calc(20vh)] pb-[calc(20vh)]">
+          <Col className="pt-[calc(20vh)] pb-[calc(20vh)]">
             <H1>
               Joshua Gao,
               <br />
               deep learning researcher at SAIL lab
             </H1>
-          </div>
-          <div className="">
+            <H2 className="flex flex-row gap-4 text-gray-500">
+              <a href="/cv.pdf" target="_blank" rel="noopener noreferrer">
+                CV
+              </a>
+              <a href="mailto:joshuakgao@gmail.com">Email</a>
+              <a
+                href="https://www.linkedin.com/in/joshua-gao"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                LinkedIn
+              </a>
+              <a
+                href="https://github.com/joshua-gao"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Github
+              </a>
+            </H2>
+          </Col>
+          <Col>
             <H3 className="mb-4">Research</H3>
             <ProjectItem title="Change Detection" year="2024" />
             <ProjectItem title="Change Detection" year="2024" />
@@ -46,7 +61,7 @@ export default function Home() {
             <ProjectItem title="Change Detection" year="2024" />
             <ProjectItem title="Change Detection" year="2024" />
             <ProjectItem title="Change Detection" year="2024" />
-          </div>
+          </Col>
         </>
       ) : (
         <>
@@ -66,13 +81,35 @@ export default function Home() {
             <ProjectItem title="Change Detection" year="2024" />
             <ProjectItem title="Change Detection" year="2024" />
           </Col>
-          <div className="fixed right-0 top-0 h-full w-2/3 flex items-center justify-center">
-            <H1>
-              Joshua Gao,
-              <br />
-              deep learning researcher at SAIL lab
-            </H1>
-          </div>
+          <Col className="fixed right-0 top-0 h-full w-3/5 flex items-start justify-center pl-6">
+            <div>
+              <H1>
+                Joshua Gao,
+                <br />
+                deep learning researcher at SAIL lab
+              </H1>
+              <H2 className="flex flex-row gap-4 text-gray-500">
+                <a href="/cv.pdf" target="_blank" rel="noopener noreferrer">
+                  CV
+                </a>
+                <a href="mailto:joshuakgao@gmail.com">Email</a>
+                <a
+                  href="https://www.linkedin.com/in/joshua-gao"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  LinkedIn
+                </a>
+                <a
+                  href="https://github.com/joshua-gao"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  Github
+                </a>
+              </H2>
+            </div>
+          </Col>
         </>
       )}
     </ScrollDiv>
