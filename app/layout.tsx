9ccda5d170bdfecfa66ec5c26c3b23357ba54766
tugonbob <57576522+tugonbob@users.noreply.github.com>
2025-03-10 02:13:22 -0500

    Added google analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,12 +18,12 @@ export default function RootLayout({
         <script
           dangerouslySetInnerHTML={{
             __html: `
-      (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-      new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-      j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-      'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-      })(window,document,'script','dataLayer','GTM-MG8V3DLK');
-    `,
+          (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','GTM-KPQDKVHF');
+        `,
           }}
         />
         <link rel="icon" href="/favicon.png" type="image/png" />
@@ -53,12 +53,8 @@ export default function RootLayout({
         <noscript
           dangerouslySetInnerHTML={{
             __html: `
-              <iframe
-          src="https://www.googletagmanager.com/ns.html?id=GTM-MG8V3DLK"
-          height="0"
-          width="0"
-          style="display:none;visibility:hidden"
-              ></iframe>
+              <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-KPQDKVHF"
+              height="0" width="0" style="display:none;visibility:hidden"></iframe>
             `,
           }}
         />
